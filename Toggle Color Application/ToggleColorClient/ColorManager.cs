985353fc49e77399d21e70756d5bb99e9c60bbff
ralf.westphal <ralf.westphal@localhost>
2010-07-25 23:46:42 +0200

    Trivial Toggle Color application

---
+++ Toggle Color Application/ToggleColorClient/ColorManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ToggleColorClient
+{
+    class ColorManager
+    {
+        private Color currentColor;
+
+
+        public void In_ToggleColor()
+        {
+            this.currentColor = this.currentColor == Color.Green
+                                    ? Color.Red
+                                    : Color.Green;
+            this.Out_CurrentColor(this.currentColor);
+        }
+
+
+        public void In_GetInitialColor()
+        {
+            this.currentColor = Color.Green;
+            this.Out_CurrentColor(this.currentColor);
+        }
+
+
+        public event Action<Color> Out_CurrentColor;
+    }
+}
---
+++ Toggle Color Application/ToggleColorClient/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToggleColorClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Out_Start();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Out_ToggleColor();
+        }
+
+
+        public void In_CurrentColor(Color color)
+        {
+            this.pictureBox1.BackColor = color;
+        }
+
+
+        public event Action Out_Start;
+        public event Action Out_ToggleColor;
+    }
+}
