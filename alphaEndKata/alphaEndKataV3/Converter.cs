bbe51138326daab713a518f19b0b2ecf50e7c997
ralf.westphal <ralf.westphal@localhost>
2011-05-16 15:57:07 +0200

    Starting alpha-end once more with tracer bullet impl

---
+++ alphaEndKata/alphaEndKataV3/Converter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace alphaEndKataV3
+{
+    public class Converter
+    {
+        public string Convert(int n10)
+        {
+            return "";
+        }
+    }
+}
