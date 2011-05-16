9919767a8d9e5dfb7a1af2e4ba1b8179ce960b9d
ralf.westphal <ralf.westphal@localhost>
2011-05-16 16:00:38 +0200

    Tracer bullet feature implemented

--- alphaEndKata/alphaEndKataV3/Converter.cs
+++ alphaEndKata/alphaEndKataV3/Converter.cs
@@ -9,7 +9,37 @@ namespace alphaEndKataV3
     {
         public string Convert(int n10)
         {
-            return "";
+            string nBased = "";
+
+            int basePower = Determine_basePower(n10);
+
+            while (basePower >= 0)
+            {
+                int factor, remainder;
+                Calc_factor_and_remainder(n10, basePower, out factor, out remainder);
+                nBased += Map_factor_to_digit(factor);
+
+                n10 = remainder;
+                basePower--;
+            }
+
+            return nBased;
+        }
+
+        private string Map_factor_to_digit(int factor)
+        {
+            return "5";
+        }
+
+        private void Calc_factor_and_remainder(int n10, int basePower, out int factor, out int remainder)
+        {
+            factor = 5;
+            remainder = 0;
+        }
+
+        private int Determine_basePower(int n10)
+        {
+            return 0;
         }
     }
 }
