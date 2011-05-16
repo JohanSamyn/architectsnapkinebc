e4cc6efe0d125daccff7c3fa9ec525ad52a451a8
ralf.westphal <ralf.westphal@localhost>
2011-05-16 15:40:49 +0200

    "Determine base power" and "Calc factor and remainder"

--- alphaEndKata/alphaEndKataV2/Converter.cs
+++ alphaEndKata/alphaEndKataV2/Converter.cs
@@ -8,7 +8,23 @@ namespace CanUDoIt
     {
         public string Convert(int n10)
         {
-            return "";
+            int basePower = Determine_basePower(n10);
+
+            int factor, remainder;
+            Divide_by_base_raised_to_basePower(n10, basePower, out factor, out remainder);
+
+
+            return "5";
+        }
+
+        private void Divide_by_base_raised_to_basePower(int n10, int basePower, out int factor, out int remainder)
+        {
+            factor = Math.DivRem(n10, (int)Math.Pow(13, basePower), out remainder);
+        }
+
+        private int Determine_basePower(int n10)
+        {
+            return (int)Math.Truncate(Math.Log10(n10)/Math.Log10(n10));
         }
     }
 }
